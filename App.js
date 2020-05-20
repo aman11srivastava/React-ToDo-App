4c9d4852d3b1d3424b3891af2507a5c6d1023fa3
Aman Srivastava <55241832+aman11srivastava@users.noreply.github.com>
2020-05-20 18:59:06 +0530

    CSS

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const TodoItem = (props) => (   //Since only single props, therefore could have written {text}
+  <li>{props.text}</li>         //and thus here we could have written {text}
+);                
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //have to use the above line because if we don t use it, then the 'this' keyword wont refer to our component and thus setState will not exist.
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo: ''});
+  }                       //newTodo: '' coz every time we save the task, and we have to enter a new task here, then we dont want/have to erase the previous task from the text box and wrte the new task, it will automatically make it empty. 
+  render() {
+    const {newTodo} = this.state;
+    const todos = this.state.todos.map((t, i) => (
+      <TodoItem key={i} text={t} />
+    ));
+    return (
+      <div className="App">
+        <h1>Simple Todo App</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="todo-input"
+            autoComplete="off"
+            type="text"
+            name="newTodo"
+            placeholder="Enter new task here!"
+            value={newTodo}
+            onChange={(e) => this.setState({[e.target.name]: e.target.value })}
+                                            //[e.target.name] can also be written as newTodo when only single input, can also be used for multiple inputs also but then it would be very tedious and we would have to write a different function for each input
+          />                                
+          <button
+            type="submit"
+            className="save-button"
+          >
+            SAVE
+          </button>
+        </form>
+        <div className="todo-content">
+          <ol>
+            {todos}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

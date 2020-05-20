31b9e415e652742f9a992b23b6d9d3dc0bf2533d
Aman Srivastava <55241832+aman11srivastava@users.noreply.github.com>
2020-05-20 18:52:09 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const TodoItem = ({text}) => (
-  <li>{text}</li>
-);
+const TodoItem = (props) => (   //Since only single props, therefore could have written {text}
+  <li>{props.text}</li>         //and thus here we could have written {text}
+);                
 
 class App extends Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class App extends Component {
     e.preventDefault();
     const todos = [...this.state.todos, this.state.newTodo];
     this.setState({todos, newTodo: ''});
-  }
+  }                       //newTodo: '' coz every time we save the task, and we have to enter a new task here, then we dont want/have to erase the previous task from the text box and wrte the new task, it will automatically make it empty. 
   render() {
     const {newTodo} = this.state;
     const todos = this.state.todos.map((t, i) => (
@@ -33,10 +33,12 @@ class App extends Component {
             autoComplete="off"
             type="text"
             name="newTodo"
-            placeholder="What needs to be done?"
+            placeholder="Enter new task here!"
             value={newTodo}
             onChange={(e) => this.setState({[e.target.name]: e.target.value })}
-          />
+                                            //[e.target.name] can also be written as newTodo when only single input, can also be used for multiple inputs also but then it would be very tedious and we would have to write a different function for each input
+          />                                
+
           <br></br>
           <br></br>
           <button
